2f0ea466ed8685545db68f5e4e3aca26695d3c4c
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-21 22:38:52 -0400

    lesson one comPLETE

--- express_lesson_one/routes/index.js
+++ express_lesson_one/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express',
+                        users: [
+                          'Jeffrey', 'Jennifer', 'Joan', 'Sophia', 'Nilla', 'Marley', 'Buddy', 'Waylan', 'Colin', 'Sallie', 'Ella-Blue'
+                                ],
+                        cats: [
+                          {name: 'Nelson'},
+                          {name: 'Chloe'},
+                          {name: 'Miss Kitty'},
+                          {name: 'Gypsy'},
+                          {name: 'Cougar Anne'},
+                          {name: 'Mamma Cat'},
+                          {name: 'Sallie'},
+                          {name: 'Waylan'}
+                        ]
+                              });
 });
 
 module.exports = router;
