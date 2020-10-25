03cae0110594f38e4c1cb4c86f414c3b6403d936
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-25 09:58:22 -0400

    a runthrough of lesson two, complete.

--- express_lesson_two/routes/index.js
+++ express_lesson_two/routes/index.js
@@ -4,18 +4,28 @@ var router = express.Router();
 const pets = ['Marley', 'Buddy', 'Waylan', 'Sallie', 'Nilla'];
 
 /* Remember CRUD - Create, Read, Update, Delete is the same as POST, GET, PUT, DELETE */
+// notice how POST changes the data in 'pets' array.
 router.post('/', function(req, res){
-  res.send('you successfully executed a POST route!');
-});
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  let queryName = req.query.pet;
-  if(pets.includes(queryName)) {
-    res.send(`Yep, that's a  ${queryName} alright!` );
+  let bodyPet = req.body;
+  if(pets.includes(bodyPet.pet)){
+    res.send(`already have a ${bodyPet.pet}, thanks!`)
   }else{
-    res.send(`${queryName} is not here`);
+    pets.push(bodyPet.pet);
+    // res.send(pets);
+    res.render('pets', {title: "Amazing Pets", pets})
   }
   
+});
+/* GET home page. */
+//notice how GET simply fetches a view. Here we can dynamically choose what to display.
+router.get('/', function(req, res, next) {
+  // let queryName = req.query.pet;
+  // if(pets.includes(queryName)) {
+  //   res.send(`Yep, that's a  ${queryName} alright!` );
+  // }else{
+  //   res.send(`${queryName} is not here`);
+  // }
+  res.render('index', {title: "Amazing Pets"});
 });
 
 router.put('/', function(req, res){
