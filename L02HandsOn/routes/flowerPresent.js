eccad08684e87f799b48480831f1c08621d5f487
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-25 17:51:59 -0400

    finished and working, and learned a lot, but wound up far away from the requirements!

--- L02HandsOn/routes/flowerPresent.js
+++ L02HandsOn/routes/flowerPresent.js
@@ -1,16 +1,13 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-
-/* GET home page. */
-router.get('/flowerList', function(req, res, next) {
-    res.render('flowerList', {title: 'Flower List', flowers});
-});
-
-// router.get('/flowerPresent', function(req, res, next) {
-//     res.render('index', {title: 'Flower List', flowers});
+// /* GET home page. */
+// router.get('/flowerList', function(req, res, next) {
+//     res.render('flowerList', {title: 'Flower List', flowers});
 // });
 
+// // router.get('/flowerPresent', function(req, res, next) {
+// //     res.render('index', {title: 'Flower List', flowers});
+// // });
 
-
-module.exports = router;
+// module.exports = router;
--- L02HandsOn/routes/index.js
+++ L02HandsOn/routes/index.js
@@ -13,12 +13,12 @@ Step 2 [X]
 Create an array that is named flowers and includes the following elements:
 ["Orchid", "Iris", "Hydrangea", "Amaryllis", "Dahlia", "Daffodil", "Bleeding Heart"]
 
-Step 3 []
-In the get method, check to see and respond accordingly that if you have a certain flower, send back a message saying "Yes, we have [flower] in our garden" and if you don't, respond, "Nope, we do not have [flower] in our garden, but maybe we should plant it!"
+Step 3 [X]
+In the get method, check to see and respond accordingly that if you have a certain flower, send back a message saying "Yes, we have [flower] in our garden" and if you don't, respond, "Nope, we do not have [flower] in our garden, but maybe we should plant it!".
 
 Hint! Use .includes().
 
-Step 4 []
+Step 4 [X]
 Create a post method that will add a flower that is not in the array into it and will respond, "We already have that flower, no need to add it" if it already exists in the array.
 
 Step 5 []
@@ -30,20 +30,25 @@ let flowers = ["Orchid", "Iris", "Hydrangea", "Amaryllis", "Dahlia", "Daffodil",
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let flowerReq = req.body;
   res.render('index', { title: 'My Garden' });
+});
+
+/* GET flower list - every time you need a different path in the url, you'll do it like this */
+router.get('/flowerList', function(req, res, next) {
+  res.render('flowerList', {title: 'Flower List', flowers});
 });
 
 router.post('/', function(req, res, next) {
   let flowerReq = req.body;
   if(flowers.includes(flowerReq.flower)){
-    // res.send(`sorry, we already have a ${flowerReq.flower} in the garden`);
+    // res.send(`Yes, we have ${flowerReq.flower} in our garden`);
     res.render('flowerPresent', {flower: flowerReq.flower} );
   }else{
+    // res.send(`Nope, we do not have ${flowerReq.flower} in our garden, but maybe we should plant it!`);
     flowers.push(flowerReq.flower);
     res.render('flowerList', {title: 'Flower List', flowers});
   }
-  // res.send(flowerReq);
-  //console.log(flowerReq.flower);//why this dont work?
 });
 
 module.exports = router;
