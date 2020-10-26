d56c0cdd0942b9868793fc2f76831bfec6d9c586
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-26 17:16:53 -0400

    a good start on lesson three...

---
+++ express_lesson_three/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var users = require('../models/users');
+
+/* GET home page. */
+router.get('/person/:id', function(req, res, next) {
+  let person = users.people.find(peep => {
+    return peep.id === parseInt(req.params.id);
+  });
+  res.render('index', { person });
+  console.log(req.path);
+});
+
+module.exports = router;
