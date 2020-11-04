f3f5b593b405cd05d646cdcc6dd0189ea6ee6244
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-04 15:55:51 -0500

    tweaked password and get route and it's rendering data. Beautiful!!!!

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # PRG7927
 Class folder for back-end frameworks
 
+//to change the password, so long as you’re already in Workbench, run this query:
+
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Password1!');
+FLUSH PRIVILEGES;
+
+
+
 password for work version of MySQL is T+w/yourw3f#
+work is now Password1!
+
+TODO: ^^ make this the same at home!
+
+home
 changed to Tornado+
--- express_lesson_four/routes/index.js
+++ express_lesson_four/routes/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
 
+//home
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'T0rn1d0_!',
+//   database: 'sakila'
+// });
+
+//work
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'T0rn1d0_!',
+  password: 'Password1!',
   database: 'sakila'
 });
 
@@ -20,14 +29,30 @@ connection.connect(function(err){
 const query = `SELECT * from actor LIMIT 10`;
 
 connection.query(query, (err, results) => {
-  if(err) throw err;
-  
+  if(err) {
+    console.err(err.message);
+    return;
+  }
   console.log(results);
 })
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET Actor. */
+router.get('/actor/:id', function(req, res, next) {
+  let actorId = parseInt(req.params.id);
+  console.log(actorId);
+  let idQuery = `SELECT * from actor WHERE actor_id=${actorId}`;
+  console.log(idQuery);
+
+  connection.query(idQuery, (err, result) => {
+    console.log(result);
+    if(result.length>0){
+      res.render( 'index', {
+        actor: result[0]
+      });
+    }else{
+      res.send('no such actor!');
+    }
+  });
 });
 
 module.exports = router;
