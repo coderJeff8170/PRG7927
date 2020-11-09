6f55df767e971a7c2a2a179ff404cab2be92bfbb
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-09 11:58:05 -0500

    lesson setup for five

---
+++ express_lesson_five/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
