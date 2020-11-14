8d623d3492f3858315c3eaa86e0fd8c261d95fa9
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-14 17:28:47 -0500

    paused for monday questions

--- L05HandsOn/models/category.js
+++ L05HandsOn/models/category.js
@@ -1,7 +1,5 @@
 /* jshint indent: 2 */
 
-const { DECIMAL } = require("sequelize");
-
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('category', {
     category_id: {
@@ -17,10 +15,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    default_price: {
-      type: DataTypes.DECIMAL(10,2),
-      defaultValue: 5.00
     }
   }, {
     tableName: 'category'
