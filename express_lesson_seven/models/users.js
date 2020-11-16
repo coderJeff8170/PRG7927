ca633b34c77f2e6e6b77f24165afaf3a33542347
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-16 12:49:28 -0500

    setup for lesson 7

---
+++ express_lesson_seven/models/users.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  users.init({
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Email: { 
+      type: DataTypes.STRING,
+      unique: true
+    },
+    UserName: { 
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  return users;
+};
