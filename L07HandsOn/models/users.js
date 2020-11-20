8dfdafae9937b0a71a8a05cf7e51aeff2ceb83f8
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-20 17:27:02 -0500

    initial setup for hands on seven

---
+++ L07HandsOn/models/users.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  users.init({
+    UserId: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    Password: DataTypes.STRING,
+    Admin: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    },
+    Deleted: {
+      type: DataTypes.BOOLEAN,
+      default: false
+    }
+  }, {
+    sequelize,
+    modelName: 'users',
+  });
+  return users;
+};
---
+++ L07HandsOn/routes/users.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// User signup: /users/signup GET & POST
+
+// User login: /users/login GET & POST
+
+// Show user profile: /users/profile GET
+
+// Show a list of users: /users GET
+// (if logged in and admin)
+
+// Show a specific user: /users/:id GET
+
+// Delete user: /users/:id DELETE (only if logged in and admin)
+
+module.exports = router;
