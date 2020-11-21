902c2b18609a0cbfc7262182c9a07b5a8fcab7ad
Jeff Andrews <jeff@paintedbyjeff.com>
2020-11-21 12:13:50 -0500

    Most all routes and views created ready for passport installating for auth purposes...

--- L07HandsOn/routes/users.js
+++ L07HandsOn/routes/users.js
@@ -42,27 +42,77 @@ router.get('/login', function(req, res, next){
 })
 
 //POST login information
-
 //show individual user profile
+router.post('/login', function(req, res, next){
+    models.users.findOne({
+        where: {
+            Username: req.body.username,
+            Password: req.body.password
+        }
+    })
+    .then(user => {
+        if(user){
+            res.render('profile', {
+                firstname: user.FirstName,
+                lastname: user.LastName,
+                email: user.Email,
+                username: user.Username,
+            });
+        }else{
+            res.send('wrong login info');
+        }
+    });
+});
+
+
 
 // Show a list of users: /users GET
 // (if logged in and admin)
 router.get('/', function(req, res, next){
     //remember, passport is gonna add a user property to the req
-    if(req.user && req.user.admin){
-        models.users.findAll({})
-        .then(users => {
-            res.render('/users', {
-                users: users
+        models.users.findAll()
+        .then(allUsers => {
+            res.render('users', {
+                users: allUsers
             });
         });
-    }else{
-        res.send('invalid request');
-    }
+
 });
 
 // Show a specific user: /users/:id GET
+router.get('/:id', function(req, res, next){
+    models.users.findByPk(parseInt(req.params.id))
+    .then(user => {
+        if(user){
+            res.render('profile', {
+                firstname: user.FirstName,
+                lastname: user.LastName,
+                email: user.Email,
+                username: user.Username
+            })
+        }else{
+            res.send('something went awry!')
+        }
+    });
+});
 
 // Delete user: /users/:id DELETE (only if logged in and admin)
+router.delete('/:id', function(req, res, next){
+    console.log("button works");
+})
+
+router.delete('/:id', function (req, res, next) {
+    let userId = parseInt(req.params.id);
+    models.users
+    .destroy( { where: { UserId : userId } })
+    .then(result => {
+      //just redirects back to all actors, since this actor no longer exists!
+      res.redirect('/actors');
+    })
+    .catch(err => {
+      res.status(400);
+      res.send("You're having trouble deleting this person!")
+    })
+  });
 
 module.exports = router;
