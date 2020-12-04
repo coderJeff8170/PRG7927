74d1e433b59fdd96c11b14301969efb1e16b80d7
Jeff Andrews <jeff@paintedbyjeff.com>
2020-12-04 12:59:28 -0500

    Lesson 10 model setup 1

---
+++ L10HandsOn/migrations/1-initial_migration.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "posts", deps: []
+ * createTable "users", deps: []
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "initial_migration",
+    "created": "2020-12-04T17:58:34.602Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+        fn: "createTable",
+        params: [
+            "posts",
+            {
+                "PostId": {
+                    "type": Sequelize.INTEGER,
+                    "field": "PostId",
+                    "primaryKey": true,
+                    "autoIncrement": true,
+                    "allowNull": false
+                },
+                "PostTitle": {
+                    "type": Sequelize.STRING,
+                    "field": "PostTitle"
+                },
+                "PostBody": {
+                    "type": Sequelize.STRING,
+                    "field": "PostBody"
+                },
+                "UserId": {
+                    "type": Sequelize.INTEGER,
+                    "field": "UserId"
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                }
+            },
+            {}
+        ]
+    },
+    {
+        fn: "createTable",
+        params: [
+            "users",
+            {
+                "UserId": {
+                    "type": Sequelize.INTEGER,
+                    "field": "UserId",
+                    "primaryKey": true,
+                    "autoIncrement": true,
+                    "allowNull": false
+                },
+                "FirstName": {
+                    "type": Sequelize.STRING,
+                    "field": "FirstName"
+                },
+                "LastName": {
+                    "type": Sequelize.STRING,
+                    "field": "LastName"
+                },
+                "Username": {
+                    "type": Sequelize.STRING,
+                    "field": "Username",
+                    "unique": true
+                },
+                "Password": {
+                    "type": Sequelize.STRING,
+                    "field": "Password"
+                },
+                "Email": {
+                    "type": Sequelize.STRING,
+                    "field": "Email",
+                    "unique": true
+                },
+                "Admin": {
+                    "type": Sequelize.BOOLEAN,
+                    "field": "Admin",
+                    "defaultValue": false
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                }
+            },
+            {}
+        ]
+    }
+];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
---
+++ L10HandsOn/models/posts.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  posts.init({
+    PostId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    PostTitle: DataTypes.STRING,
+    PostBody: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'posts',
+  });
+  return posts;
+};
